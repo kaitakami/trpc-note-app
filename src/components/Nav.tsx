d6652fc0df9765fe8a0ec072e359a440fd2860b9
Kai Takami <kaitakami@protonmail.com>
2023-02-01 22:01:26 +0900

    Make some small changes to the nav design

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -5,7 +5,7 @@ const Nav = () => {
     <nav className="flex gap-12 md:gap-20 px-6
      md:px-14 py-5 md:border justify-between border-slate-700 rounded-2xl md:mt-8 md:fixed w-full max-w-4xl">
       <Link className="my-auto" href="/"><h1 className="text-xl md:text-2xl font-bold">Notes app</h1></Link>
-      <Link className="my-auto border rounded-md p-3 transition-colors hover:bg-transparent bg-slate-700 border-slate-800" href="/new">New Note ➕</Link>
+      <Link className="my-auto border rounded-md p-3 transition-colors hover:bg-transparent bg-slate-700 border-slate-800" href="/new">New Note</Link>
     </nav>
   )
 }
